62307f12f1a555927251a384218894cf23bbe76e
Priyanshu Agarwal <134536864+someoneskilled@users.noreply.github.com>
2024-09-08 08:12:27 +0530

    Add files via upload

---
+++ p4.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+# Create an array
+
+array = np.array([[1, 2, 3], [4, 5, 6]])
+
+# 1. Type of array
+
+array_type = type(array)
+
+print("Type of array:", array_type)
+
+# 2. Shape of array
+
+array_shape = array.shape
+
+print("Shape of array:", array_shape)
+
+# 3. Type of elements in array
+
+element_type = array.dtype
+
+print("Type of elements in array:", element_type)
+
